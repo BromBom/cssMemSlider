c033d3b062bf818777234677e7a0cc4068d55bbb
Ihar Levachkov <pokerlis@gmail.com>
2021-10-17 11:17:55 +0300

    feat: add js code for slider 2021-10-17 11:17:55

---
+++ cssMemSlider/js/slider.js
@@ -0,0 +1,74 @@
+const buttons = document.querySelectorAll('.slider-button');
+const slides = document.querySelectorAll('.slide');
+const texts = document.querySelectorAll('.text');
+
+let isEnabled = true;
+let slideIndex = 0;
+
+
+function changeSlide(x) {
+    slideIndex = (x + slides.length) % slides.length;
+}
+
+function hideSlide(direction) {
+    isEnabled = false;
+    slides[slideIndex].classList.add(direction);
+    texts[slideIndex].classList.add(direction);
+    slides[slideIndex].addEventListener('animationend', function() {
+        this.classList.remove('active', direction);
+    });
+    texts[slideIndex].addEventListener('animationend', function() {
+        this.classList.remove('active-text', direction);
+    });
+}
+
+function showSlide(direction) {
+    slides[slideIndex].classList.add('next', direction);
+    texts[slideIndex].classList.add('next-text', direction);
+    slides[slideIndex].addEventListener('animationend', function() {
+        this.classList.remove('next', direction);
+        this.classList.add('active');
+        isEnabled = true;
+    })
+    texts[slideIndex].addEventListener('animationend', function() {
+        this.classList.remove('next-text', direction);
+        this.classList.add('active-text');
+        isEnabled = true;
+    })
+}
+
+function previousSlide(x) {
+    hideSlide('to-right');
+    changeSlide(x - 1);
+    showSlide('from-left');
+    for (let i = 0; i < 5; i++) {
+        buttons[i].className = buttons[i].className.replace(' active-slider-button', '');
+    }
+    buttons[slideIndex].className += ' active-slider-button';
+}
+
+function nextSlide(x) {
+    hideSlide('to-left');
+    changeSlide(x + 1);
+    showSlide('from-right');
+    for (let i = 0; i < 5; i++) {
+        buttons[i].className = buttons[i].className.replace(' active-slider-button', '');
+    }
+    buttons[slideIndex].className += ' active-slider-button';
+}
+
+buttons[0].addEventListener('click', function() {
+    previousSlide(1);
+});
+buttons[1].addEventListener('click', function() {
+    previousSlide(2);
+});
+buttons[2].addEventListener('click', function() {
+    previousSlide(3);
+});
+buttons[3].addEventListener('click', function() {
+    previousSlide(4);
+});
+buttons[4].addEventListener('click', function() {
+    previousSlide(0);
+});
